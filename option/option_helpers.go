f89e58cb6443c138ae6ddd7e07dfdb6ae211c836
zanehy <zanehy@foxmail.com>
2025-12-18 22:39:13 +0800

    feat: add func nolint unused

--- option/option_helpers.go
+++ option/option_helpers.go
@@ -2,27 +2,23 @@ package option
 
 import "time"
 
-//nolint:unused
 func StringPresent(s *string) bool {
 	return s != nil && *s != ""
 }
 
-//nolint:unused
 func IntPresent(i *int) bool {
 	return i != nil
 }
 
-//nolint:unused
 func BoolPresent(b *bool) bool {
 	return b != nil
 }
 
-//nolint:unused
 func TimePresent(t *time.Time) bool {
 	return t != nil && !t.IsZero()
 }
 
-//nolint:unused // GetString 返回字符串值及是否存在（非 nil 且非空）
+// GetString 返回字符串值及是否存在（非 nil 且非空）
 func GetString(s *string) (string, bool) {
 	if s == nil || *s == "" {
 		return "", false
@@ -30,7 +26,7 @@ func GetString(s *string) (string, bool) {
 	return *s, true
 }
 
-//nolint:unused // GetInt 返回 int 值及是否存在（非 nil）
+// GetInt 返回 int 值及是否存在（非 nil）
 func GetInt(i *int) (int, bool) {
 	if i == nil {
 		return 0, false
@@ -38,7 +34,7 @@ func GetInt(i *int) (int, bool) {
 	return *i, true
 }
 
-//nolint:unused // GetBool 返回 bool 值及是否存在（非 nil）
+// GetBool 返回 bool 值及是否存在（非 nil）
 func GetBool(b *bool) (bool, bool) {
 	if b == nil {
 		return false, false
@@ -46,7 +42,7 @@ func GetBool(b *bool) (bool, bool) {
 	return *b, true
 }
 
-//nolint:unused // GetTime 返回 time.Time 值及是否存在（非 nil 且非零值）
+// GetTime 返回 time.Time 值及是否存在（非 nil 且非零值）
 func GetTime(t *time.Time) (time.Time, bool) {
 	if t == nil || t.IsZero() {
 		return time.Time{}, false
